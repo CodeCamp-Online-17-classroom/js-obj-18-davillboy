091c91bfa09d0600975e5ff4ede99aa2526e237f
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 18:39:29 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // write code here
+บรรทัดที่มี * ให้ผลลัพธ์เป็นอะไร เพราะอะไร
+
+let user = {
+  name: 'John',
+  sayHi: function () {
+    console.log(this.name);
+  }
+};
+
+(user.sayHi)(); // *
+
+ผลลัพธ์
+John 
+เรียกใช้ user.sayHi() ในที่นี้ this จะชี้ไปที่ user object ซึ่งมี property name คือ 'John' ดังนั้นเมื่อเรา log this.name ก็จะได้ผลลัพธ์เป็น 'John'
